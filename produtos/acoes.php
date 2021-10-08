3b992a6fad6ec7ba497464473a859f0b3b74a599
MATEUS BISPO RAMOS <21192976@CFP127ALUNO.LOCAL>
2021-10-08 11:26:49 -0300

    Função adicionar Produto

--- produtos/acoes.php
+++ produtos/acoes.php
@@ -17,6 +17,22 @@ switch($_POST["acao"]){
         // UPLOAD DO ARQUIVO
         move_uploaded_file($_FILES["foto"]["tmp_name"], "./fotos/$novoNome");
 
+        $descricao = $_POST["descricao"];
+        $peso = $_POST["peso"];
+        $quantidade = $_POST["quantidade"];
+        $cor = $_POST["cor"];
+        $tamanho = $_POST["tamanho"];
+        $valor = $_POST["valor"];
+        $desconto = $_POST["desconto"];
+        $categoriaId = $_POST["categoria"];
+
+        // INSTRUÇÃO SQL DE INSERÇÃO DE DADOS
+        $sql = "INSERT INTO tbl_produto (descricao, peso, quantidade, cor, tamanho, valor, desconto, imagem, categoria_id)
+        VALUES ('$descricao', $peso, $quantidade, '$cor', '$tamanho', $valor, $desconto, '$novoNome', '$categoriaId')";
+
+        $resultado = mysqli_query($conexao, $sql);
+
+        header("location: ./novo/index.php");
 
         break;
 }
