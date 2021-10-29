99f533520c3199c058cbdaa1f1011f7b36fa5724
MATEUS BISPO RAMOS <21192976@CFP127ALUNO.LOCAL>
2021-10-29 11:28:37 -0300

    criptografia da senha

--- componentes/header/acoesLogin.php
+++ componentes/header/acoesLogin.php
@@ -12,12 +12,12 @@
         $resultado = mysqli_query($conexao, $sql);
         $dadosUsuario = mysqli_fetch_array($resultado);
 
-        if(isset($dadosUsuario["usuario"]) && isset($dadosUsuario["senha"])){
+        if(isset($dadosUsuario["usuario"]) && isset($dadosUsuario["senha"]) && password_verify($senha, $dadosUsuario["senha"])){
             $_SESSION["usuarioId"] = $dadosUsuario["id"];
             $_SESSION["nome"] = $dadosUsuario["nome"];
             header("location: ../../produtos/index.php");
         } else{
-
+            header("location: ../../produtos/index.php");
         }
 
     }
---
+++ gerador_senha.php
@@ -0,0 +1,4 @@
+<?php
+    $senha = password_hash('123', PASSWORD_DEFAULT);
+    echo $senha;
+?>
